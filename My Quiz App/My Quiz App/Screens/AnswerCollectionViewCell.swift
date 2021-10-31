4d620545fdd28cf441d40715b687d5a49ba1c4b8
VietanhDoan <doanvietanhyolo97@gmail.com>
2021-10-31 10:06:36 +0700

    add Result screen

---
+++ My Quiz App/My Quiz App/Screens/AnswerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerCollectionViewCell.swift
+//  My Quiz App
+//
+//  Created by Viet Anh on 31/10/2021.
+//
+
+import UIKit
+
+class AnswerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- My Quiz App/My Quiz App/Screens/PlayViewController.swift
+++ My Quiz App/My Quiz App/Screens/PlayViewController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class PlayViewController: UIViewController {
-
+class PlayViewController: UIViewController{
+    
+    @IBOutlet weak var labelQuestion: UILabel!
+    @IBOutlet weak var collectionViewAnswer: UICollectionView!
+    @IBOutlet weak var buttonSubmitAnswer: CustomButton!
+    
+    @IBAction func submitAnswer(_ sender: Any) {
+        let resultController = ResultViewController()
+        self.navigationController?.pushViewController(resultController, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+//        collectionViewAnswer.delegate = self
+//        collectionViewAnswer.dataSource = self
+    }
+    
+    func changeToNextQuestion() -> Void {
+        //
     }
 
 
@@ -27,3 +41,17 @@ class PlayViewController: UIViewController {
     */
 
 }
+
+//extension PlayViewController: UICollectionViewDelegate, UICollectionViewDataSource  {
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionViewAnswer.dequeueReusableCell(
+//            withReuseIdentifier: "AnswerCollectionViewCell",
+//            for: indexPath)
+//        return cell
+//    }
+//}
